fed19c7618ef18a476a8007655397c89a674e4db
GiselleBarbosa <g88barbosa@gmail.com>
2024-04-01 11:44:06 -0300

    adicionada rota padrao de inicializacao

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,17 +5,22 @@ import { UserComponent } from './pages/user/user.component';
 import { AuthGuard } from './services/auth-guard.service';
 
 export const routes: Routes = [
-    {
-        path: "login",
-        component: LoginComponent
-    },
-    {
-        path: "signup",
-        component: SignUpComponent
-    },
-    {
-        path: "user",
-        component: UserComponent,
-        canActivate: [AuthGuard]
-    }
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full',
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: 'signup',
+    component: SignUpComponent,
+  },
+  {
+    path: 'user',
+    component: UserComponent,
+    canActivate: [AuthGuard],
+  },
 ];
